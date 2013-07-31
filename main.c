72c94c7e15a77b453ae3bd2daa108030d4004f28
Chao LU <loochao@gmail.com>
2013-07-30 21:53:09 -0400

    push test

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
 
 int main()
 {
     return 0;
+}
+
+void array_init ()
+{
+  void * p;
+  int n = 200;
+  int i;
+  p = (int *) malloc(n * sizeof(int));
 }
