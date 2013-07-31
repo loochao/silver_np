2d4be879860f67d35a051b74d2f7e168ef980140
ryan <ryan@nat-oitwireless-inside-vapornet100-b-7078.princeton.edu>
2013-07-30 21:47:15 -0400

    ryans commit after alloc2d

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 
+#include <stdlib.h>
+#include <stdio.h>
+
+void ** alloc_2d(int nx, int ny)
+{
+    int i;
+    void ** array = malloc(sizeof(double) * nx);
+    for (i=0; i<nx; i++)
+        array[i] = malloc(sizeof(double) * ny);
+
+    return array;
+}
+
+
 int main()
 {
+
+    int nx = 100;
+    int ny = 100;
+
+    double ** phi = (double **) alloc_2d(nx, ny);
+
     return 0;
+
 }
+
